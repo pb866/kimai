0fcdde6cd3fe251de83920215049704506cad011
Peter Bräuer <pb866.git@gmail.com>
2022-05-31 00:59:22 +0200

    Add module dataimport with class TimeLog
    Add new module dataimport with class TimeLog and import to kimbal in
    __init__.py.

--- kimbal/__init__.py
+++ kimbal/__init__.py
@@ -4,3 +4,6 @@ from ._version import __version__
 # Import classes and logging variables
 from .main import Kimai
 from .main import CustomFormatter, logger, ch
+
+# Import kimbal modules
+from kimbal import dataimport
